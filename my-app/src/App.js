5fc5e00fb2b58e8c5a2fd0b3584b704dd9036a81
jkuenzinger <jkuenzinger228@gmail.com>
2020-03-10 12:26:13 -0700

    created cra and installed dependcies, set up basic file structure and calss component structure

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+import UserCard from './components/Usercard';
+import './App.css';
+
+class App extends React.Component {
+ state={
+
+ };
+ render(){
+ return (
+    <div className="main-container">
+     <UserCard/>>
+    </div>
+    );
+  }
+}
+export default App;
